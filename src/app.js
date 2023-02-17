635b4742d3f9120b051b687824c884b97fe64599
CacoMoore <caroavello.m@gmail.com>
2023-02-17 21:29:14 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let signs = ["♦", "♥", "♠", "♣"];
+
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSign = signs[Math.floor(Math.random() * signs.length)];
+
+  let finalNumber = document.querySelector(".number");
+  let finalSign = document.querySelector(".top");
+  let finalSign2 = document.querySelector(".final");
+
+  if (randomSign == signs[0] || randomSign == signs[1]) {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".final").style.color = "red";
+  }
+  finalNumber.innerHTML = randomNumber;
+  finalSign.innerHTML = randomSign;
+  finalSign2.innerHTML = randomSign;
 };
